1cf51ddffbe89996a0aac6088dd2464d48548f23
hamzamansoorch <hamzamansoorch@gmail.com>
2021-12-06 17:03:02 +0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-task4';
+export class AppComponent implements OnInit {
+  userForm: FormGroup
+  listdata: any
+  contentEditable: boolean = false;
+  edit = false;
+
+  constructor(private fb: FormBuilder) {
+    this.listdata = [];
+    this.userForm = this.fb.group({
+      fname: ['', Validators.required],
+      lname: ['', Validators.required],
+      phone: ['', Validators.required],
+
+    })
+  }
+  public addItem(): void {
+    this.listdata.push(this.userForm.value);
+    this.userForm.reset();
+  }
+  public reset(): void {
+    this.userForm.reset();
+
+
+  }
+  selectDiv(): void {
+    this.edit = !this.edit;
+    this.contentEditable = this.edit;
+
+
+
+  }
+  removeItem(element: any) {
+    this.listdata.forEach((value: any, index: any) => {
+      if (value == element) {
+        this.listdata.splice(index, 1);
+      }
+    });
+  }
+  ngOnInit() {
+
+  }
 }
